2618b82bfe9849cef35091c47957c2ad58151440
nicolas <nicolas.lidin@gmail.com>
2025-01-31 15:10:33 +0100

    chore: try size 100px

--- src/components/Atoms/AvatarRound/AvatarRound.vue
+++ src/components/Atoms/AvatarRound/AvatarRound.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-img alt="nicolas_pp" :src="props.imgPath"
             class="circle" height="100" width="100"
-            sizes="(max-width: 500px) 100px, (min-width: 501px) 100px"
+            sizes="100px"
             :modifiers="{ width: 100, height: 100, fit: 'cover' }"></nuxt-img>
 </template>
 
